0e881a06481f682204f953dea70b27ac4ce65b5c
Ani Nandi <anin@alleninstitute.org>
2018-08-02 15:53:14 -0700

    Add dendrite type to the metadata

--- Human/PC/save_cell_metadata.py
+++ Human/PC/save_cell_metadata.py
@@ -7,30 +7,34 @@ Created on Wed Aug  1 12:57:55 2018
 """
 
 import json
+import os
 
-
+metadata_path = 'cell_metadata.json'
 
 def save_cell_info(*args):
     """Create and save a dictionary with all the metadata"""
     
-    cell_keys = ['Cell_id', 'Model_id', 'Species', 'Cre_line', 'Area', 'Layer']
+    cell_keys = ['Cell_id', 'Model_id', 'Species', 'Cre_line', 'Area', 'Layer','Dendrite_type']
     cell_info_dict = {}
     for i,arg in enumerate(args):
         cell_info_dict[cell_keys[i]] = arg
         
-    with open('cell_metadata.json','w') as cell_info:
+    with open(metadata_path,'w') as cell_info:
         json.dump(cell_info_dict,cell_info,indent=4)      
         
 def main():
-    cell_id = raw_input('Enter the cell_id : ')
-    model_id = raw_input('Enter the model_id : ')
-    species = raw_input('Enter the species : ')
-    Cre_line = raw_input('Enter the Cre-line : ')
-    Area = raw_input('Enter the Area : ')
-    layer = raw_input('Enter the layer number: ')
-    
-    save_cell_info(cell_id, model_id, species, Cre_line, Area, layer)
-    
+    if not os.path.exists(metadata_path):
+        cell_id = raw_input('Enter the cell_id : ')
+        model_id = raw_input('Enter the model_id : ')
+        species = raw_input('Enter the species : ')
+        cre_line = raw_input('Enter the Cre-line : ')
+        area = raw_input('Enter the Area : ')
+        layer = raw_input('Enter the layer number : ')
+        dendrite_type = raw_input('Enter dendrite type : ')
+        
+        save_cell_info(cell_id, model_id, species, cre_line, area, 
+                                                   layer, dendrite_type)
+        
     
     
 if __name__ == '__main__':
