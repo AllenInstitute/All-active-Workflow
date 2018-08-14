a8e7b9965e4269e18fb8818a9fff82ca8aa0fd31
Ani Nandi <anin@alleninstitute.org>
2018-08-13 17:07:59 -0700

    Keeping the cell_types and preprocessed/ directories intact

--- Human/GC/Ih_step/launch_stage2.sh
+++ Human/GC/Ih_step/launch_stage2.sh
@@ -18,14 +18,14 @@ fi
 
 python Optim_Main.py --checkpoint checkpoints/seed1.pkl --short_analyse
 echo "Saving the Optimized parameters for the next stage"
-rm -rf preprocessed/ 
+#rm -rf preprocessed/ 
 
 
 # Launch the All-active optimization (Stage 2)
 
 cp -r cell_types/ $ALL_ACTIV_DIR/
 cp cell_id.txt $ALL_ACTIV_DIR/
-rm -rf cell_types
+#rm -rf cell_types
 mv fit_opt.json $ALL_ACTIV_DIR/cell_types/
 cp -r $ALL_ACTIV_REPO/* $ALL_ACTIV_DIR/
 cd $ALL_ACTIV_DIR
@@ -33,4 +33,4 @@ python starter_optim.py
 nrnivmodl modfiles/
 echo "Launching Stage 2 Opimization"
 RES_2=$(sbatch start_haswell.sh) && RES_3=$(sbatch --dependency=afternotok:${RES_2##* } restart_haswell.sh)  # sbatch command goes here
-rm -rf preprocessed/ 
+#rm -rf preprocessed/ 
