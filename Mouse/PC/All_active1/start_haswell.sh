16d628cfcf139363dab00acb52d54c826af30873
Ani Nandi <anin@alleninstitute.org>
2018-08-21 11:28:39 -0700

    Consecutively submit jobs until enough generations

--- Mouse/PC/All_active1/start_haswell.sh
+++ Mouse/PC/All_active1/start_haswell.sh
@@ -8,7 +8,25 @@
 #SBATCH --mail-user=anirban.nandi@wustl.edu
 #SBATCH --mail-type=ALL
 #SBATCH -J Stage2
+#SBATCH --signal=B:USR1@120
 
+run_dependent_script() { 
+func="$1" ; shift 
+for sig ; do 
+trap "$func $sig" "$sig" 
+done 
+} 
+
+# trap function to launch the passive+Ih optimization (Stage 2)
+func_trap() { 
+mv checkpoints checkpoints_old
+mv checkpoints_backup checkpoints 
+sbatch restart_haswell.sh
+#echo Launching Stage2 through xfer job because of timeout
+} 
+
+#submit launch script upon signal USR1 
+run_dependent_script func_trap USR1 
 
 set -e
 set -x
@@ -46,3 +64,9 @@ for seed in 1; do
 done
 
 wait $pids
+
+
+# If job finishes in time analyze result
+mv checkpoints checkpoints_old
+mv checkpoints_backup checkpoints 
+sh analyse.sh
