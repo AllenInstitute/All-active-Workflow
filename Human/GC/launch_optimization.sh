bb2ec60b443be101ae97a63bd97a8f9b2dcf933b
Ani Nandi <anin@alleninstitute.org>
2018-08-28 11:54:28 -0700

    Correspond the jobname to cell name automatically

--- Human/GC/launch_optimization.sh
+++ Human/GC/launch_optimization.sh
@@ -14,9 +14,14 @@ mkdir $PASS_DIR
 cp -r cell_types/ $PASS_DIR/
 cp -r $PASSIVE_REPO/* $PASS_DIR/
 cd $PASS_DIR
+python set_features_passive.py
+python set_params_passive.py
 python starter_optim.py
-echo $CELL_ID > cell_id.txt
+STAGE="_STAGE0"
+JOBNAME=$CELL_ID$STAGE
+sed -i -e "s/Stage0/$JOBNAME/g" start_haswell.sh
 echo "Launching Stage 0 Opimization"
 RES_0=$(sbatch start_haswell.sh)  # sbatch command goes here
 echo ${RES_0##* } > Job_0.txt
 echo $PARENT_DIR > pwd.txt
+echo $CELL_ID > cell_id.txt
