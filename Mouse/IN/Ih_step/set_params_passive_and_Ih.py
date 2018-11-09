5a61f706f0a2729932dc58bab0e67c940dd038fc
Ani Nandi <anin@alleninstitute.org>
2018-11-09 11:10:47 -0800

    Adding Mouse IN

---
+++ Mouse/IN/Ih_step/set_params_passive_and_Ih.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 31 13:54:54 2018
+
+@author: anin
+"""
+
+import json
+
+passive_and_Ih_params = {   
+                'cm' : {'section' : ['soma', 'apic', 'dend', 'axon'],
+                          'bounds':{'soma':[1e-1,10], 'apic':[1e-1,10], 'dend' : [1e-1,10],
+                                    'axon':[1e-1,10]},
+                        },
+                'Ra' : {'section' : ['all'],
+                        'bounds' : {'all':[50, 200]}
+                        },
+                'g_pas' : {'section' : ['all'],
+                        'bounds' : {'all':[1e-7, 1e-2]}
+                        },
+                'e_pas' : {'section' : ['all'],
+                        'bounds' : {'all':[-110, -60]}
+                        },
+                'gbar_Ih' : {'section' : ['soma', 'apic', 'dend'],
+                        'mechanism': 'Ih',
+                        'bounds' : {'soma':[1e-7,1e-4], 'apic':[1e-7, 1e-4], 'dend' : [1e-7,1e-4]}
+                        },
+                }
+
+
+
+def main():
+    with open('passive_and_Ih_bounds.json','w') as bound_file:
+        json.dump(passive_and_Ih_params,bound_file,indent=4)   
+        
+        
+if __name__ == '__main__':
+    main()
