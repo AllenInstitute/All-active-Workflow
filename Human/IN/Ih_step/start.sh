119bc297d38abf9e31f6310181350a8acdca39a3
Ani Nandi <anin@alleninstitute.org>
2018-11-09 11:39:25 -0800

    Adding Human IN

---
+++ Human/IN/Ih_step/start.sh
@@ -0,0 +1,1 @@
+./Optim_Main.py -vv  --checkpoint seed1.pkl   --compile   --offspring_size=2    --max_ngen=2      --start
---
+++ Human/IN/Ih_step/start_bbp.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#SBATCH -p prod 
+#SBATCH -n 256
+#SBATCH -t 4:00:00
+#SBATCH -C cpu|nvme
+#SBATCH -A proj36
+#SBATCH --mail-user=anirban.nandi@wustl.edu
+#SBATCH --mail-type=ALL
+#SBATCH -J Stage1
+#SBATCH --signal=B:USR1@60
+
+run_dependent_script() { 
+func="$1" ; shift 
+for sig ; do 
+trap "$func $sig" "$sig" 
+done 
+} 
+
+# trap function to launch the passive+Ih optimization (Stage 2)
+func_trap() { 
+sbatch launch_stage2_bbp.slurm
+} 
+
+#submit launch script upon signal USR1 
+run_dependent_script func_trap USR1 
+
+
+set -e
+set -x
+
+PWD=$(pwd)
+LOGS=$PWD/logs
+mkdir -p $LOGS
+
+OFFSPRING_SIZE=512
+MAX_NGEN=50
+
+export IPYTHONDIR=${PWD}/.ipython
+export IPYTHON_PROFILE=benchmark.${SLURM_JOBID}
+
+ipcontroller --init --ip='*' --sqlitedb --ping=30000 --profile=${IPYTHON_PROFILE} &
+sleep 10
+srun -n 256 --output="${LOGS}/engine_%j_%2t.out" ipengine --timeout=3000 --profile=${IPYTHON_PROFILE} &
+sleep 10
+
+CHECKPOINTS_DIR="checkpoints"
+mkdir -p ${CHECKPOINTS_DIR}
+
+pids=""
+for seed in 1; do
+    python Optim_Main.py             \
+        -vv                                \
+        --compile                          \
+        --offspring_size=${OFFSPRING_SIZE} \
+        --max_ngen=${MAX_NGEN}             \
+        --seed=${seed}                     \
+        --ipyparallel                      \
+        --start                         \
+        --checkpoint "${CHECKPOINTS_DIR}/seed${seed}.pkl" &
+    pids+="$! "
+done
+
+wait $pids
+
+
+# Launch the passive+Ih optimization (Stage 1) once the job finishes successfully
+sbatch launch_stage2_bbp.slurm
