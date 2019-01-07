Correcting the fI curve calculation

Human/IN/All_active1/optim_analysis.py
@@ -619,7 +619,7 @@ def post_processing(checkpoint_file, responses_filename):
     # Calculating Spikerate only for LongDC (1s step currents) 
     
     stim_map_filename = 'preprocessed/StimMapReps.csv'
-    accept_stimtype_list = ['LongDC']
+    reject_stimtype_list = ['LongDCSupra','Ramp', 'ShortDC']
     stim_map = defaultdict(dict)
     with open(stim_map_filename, 'r') as stim_map_file:
         stim_map_content = stim_map_file.read()
@@ -628,7 +628,7 @@ def post_processing(checkpoint_file, responses_filename):
         if line is not '':
             stim_name, stim_type, holding_current, amplitude_start, amplitude_end, \
                 stim_start, stim_end, duration, sweeps = line.split(',')
-            if any(stim_type in stim_name for stim_type in accept_stimtype_list):
+            if not any(stim_type_iter in stim_name for stim_type_iter in reject_stimtype_list):
                 iter_dict= dict()
                 iter_dict['type'] = stim_type.strip()
                 iter_dict['hypamp'] = 1e9 * float(holding_current)
@@ -682,7 +682,7 @@ def post_processing(checkpoint_file, responses_filename):
                               if trace_dict[feature_name] is not None]
             
         if feature_values:
-            feature_mean = feature_values[0]
+            feature_mean = np.mean(feature_values)
             
         else:
             feature_mean = 0
@@ -709,8 +709,10 @@ def post_processing(checkpoint_file, responses_filename):
     
     for key,val in opt_responses.items():
         if 'soma' in key:
-            if any(stim_type in key for stim_type in accept_stimtype_list):
+            if not any(stim_type_iter in key for stim_type_iter in reject_stimtype_list):
                 stim_name = key.split('.')[0]
+                if 'DB' in stim_name:
+                    continue
                 resp_time = val['time'].as_matrix()
                 resp_voltage = val['voltage'].as_matrix()
                 stim_amp = stim_map[stim_name]['stimuli'][0]['amp']

Human/PC/All_active1/optim_analysis.py
@@ -607,7 +607,7 @@ def post_processing(checkpoint_file, responses_filename):
     # Calculating Spikerate only for LongDC (1s step currents) 
     
     stim_map_filename = 'preprocessed/StimMapReps.csv'
-    accept_stimtype_list = ['LongDC']
+    reject_stimtype_list = ['LongDCSupra','Ramp', 'ShortDC']
     stim_map = defaultdict(dict)
     with open(stim_map_filename, 'r') as stim_map_file:
         stim_map_content = stim_map_file.read()
@@ -616,7 +616,7 @@ def post_processing(checkpoint_file, responses_filename):
         if line is not '':
             stim_name, stim_type, holding_current, amplitude_start, amplitude_end, \
                 stim_start, stim_end, duration, sweeps = line.split(',')
-            if any(stim_type in stim_name for stim_type in accept_stimtype_list):
+            if not any(stim_type_iter in stim_name for stim_type_iter in reject_stimtype_list):
                 iter_dict= dict()
                 iter_dict['type'] = stim_type.strip()
                 iter_dict['hypamp'] = 1e9 * float(holding_current)
@@ -670,7 +670,7 @@ def post_processing(checkpoint_file, responses_filename):
                               if trace_dict[feature_name] is not None]
             
         if feature_values:
-            feature_mean = feature_values[0]
+            feature_mean = np.mean(feature_values)
             
         else:
             feature_mean = 0
@@ -697,10 +697,12 @@ def post_processing(checkpoint_file, responses_filename):
     
     for key,val in opt_responses.items():
         if 'soma' in key:
-            if any(stim_type in key for stim_type in accept_stimtype_list):
+            if not any(stim_type_iter in key for stim_type_iter in reject_stimtype_list):
                 stim_name = key.split('.')[0]
-                resp_time = val['time'].as_matrix()
-                resp_voltage = val['voltage'].as_matrix()
+                if 'DB' in stim_name:
+                    continue
+                resp_time = val['time'].values
+                resp_voltage = val['voltage'].values
                 stim_amp = stim_map[stim_name]['stimuli'][0]['amp']
                 trace1 = {}
                 trace1['T'] = resp_time
@@ -790,8 +792,8 @@ def post_processing(checkpoint_file, responses_filename):
         model_sweeps = []    
         model_sweep = {}
         name_loc = stim_name+'.soma.v'
-        resp_time = opt_responses[name_loc]['time'].as_matrix()
-        resp_voltage = opt_responses[name_loc]['voltage'].as_matrix()
+        resp_time = opt_responses[name_loc]['time'].values
+        resp_voltage = opt_responses[name_loc]['voltage'].values
         model_sweep['T'] = resp_time
         model_sweep['V'] = resp_voltage
         model_sweep['stim_start'] = [stim_map[stim_name]['stimuli'][0]['delay']]

Mouse/PC/All_active1/optim_analysis.py
@@ -664,7 +664,7 @@ def post_processing(checkpoint_file, responses_filename):
         if line is not '':
             stim_name, stim_type, holding_current, amplitude_start, amplitude_end, \
                 stim_start, stim_end, duration, sweeps = line.split(',')
-            if not any(stim_type in stim_name for stim_type in reject_stimtype_list):
+            if not any(stim_type_iter in stim_name for stim_type_iter in reject_stimtype_list):
                 iter_dict= dict()
                 iter_dict['type'] = stim_type.strip()
                 iter_dict['hypamp'] = 1e9 * float(holding_current)
@@ -718,7 +718,7 @@ def post_processing(checkpoint_file, responses_filename):
                               if trace_dict[feature_name] is not None]
             
         if feature_values:
-            feature_mean = feature_values[0]
+            feature_mean = np.mean(feature_values)
             
         else:
             feature_mean = 0
@@ -745,7 +745,7 @@ def post_processing(checkpoint_file, responses_filename):
     
     for key,val in opt_responses.items():
         if 'soma' in key:
-            if not any(stim_type in key for stim_type in reject_stimtype_list):
+            if not any(stim_type_iter in key for stim_type_iter in reject_stimtype_list):
                 stim_name = key.split('.')[0]
                 if 'DB' in stim_name:
                     continue