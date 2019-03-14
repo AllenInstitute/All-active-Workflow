5bcadbe8309671e34e4f07da5b9ba20ddb6d2614
Ani Nandi <anirban6908@gmail.com>
2019-03-14 09:19:43 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(name='ateamopt',
+      version='0.1',
+      description='Ateam all-active optimization toolbox',
+      author='Ani Nandi',
+      author_email='anin@alleninstitute.org',
+      packages=find_packages()
+      )
+
